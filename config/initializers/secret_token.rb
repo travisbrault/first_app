c54b637575c7dac325733e02609f5bd7bcb7a5d8
travisbrault <brault.travis@gmail.com>
2013-06-04 15:47:56 -0400

    first commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = '439b42b40e9640a563cef4cb03a0671c7dbcf1074455d82b882208bdb25d588c5aed276bbde0a6997a9d3e7418d1cae68532a336494e56b568ffc0806033e9f4'
+FirstApp::Application.config.secret_token = '80b73bd086063294e1f1f26ebddaf231c2494fbf2a163c1b9296da983cfd4584dd8b46b1b3c5b966f5b455d8f45822c1181577a407cbbd96e3805f177f99154d'
